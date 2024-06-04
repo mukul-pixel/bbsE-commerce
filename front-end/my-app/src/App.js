a71dd364172ec79daf973a48c188c3e80e0c4ff4
Mukul <mukul8756@gmail.com>
2024-06-04 17:53:29 +0530

    front-end correction after deployment

--- front-end/my-app/src/App.js
+++ front-end/my-app/src/App.js
@@ -16,6 +16,8 @@ import { AdminViewProduct } from "./Pages/AdminViewProduct";
 import { AdminViewUser } from "./Pages/AdminViewUser";
 import { Checkout } from "./Components/Checkout";
 import { Products } from "./Components/Products";
+import { AdminHome } from "./Components/AdminHome";
+import { AddProduct } from "./Components/AddProduct";
 
 
 function App() {
@@ -25,8 +27,8 @@ function App() {
       <Routes>
         {/* {role === 'admin'?
         <> */}
-        <Route path="admin" element={<AdminDashBoard />}></Route>
-        <Route path="addProduct" element={<AdminAddProduct />}></Route>
+        <><Route path="admin" element={<AdminHome />}></Route>
+        <Route path="addProduct" element={<AddProduct />}></Route>
         <Route path="/" element={<UserHome />}></Route>
         <Route path="about" element={<UserAbout />}></Route>
         <Route path="contact" element={<UserContact />}></Route>
@@ -42,6 +44,7 @@ function App() {
         <Route path="checkout" element={<Checkout/>}></Route>
         <Route path="products" element={<Products/>}></Route>
         <Route path="*" element={<PageNotFound/>}></Route>
+        </>
         {/* </>:
         <>
         <Route path="/" element={<UserHome />}></Route>
